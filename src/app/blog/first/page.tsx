84bc677252e97eb18ea3969b7804556f76831914
Scott Jones <info@netgurusolutions.co.uk>
2025-06-03 10:51:39 +0100

    feat: implement nested routes
    - Add /blog route with nested /first and /second subroutes
    
    - Implement dynamic route segments
    
    - Add layout and page components for nested structure

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FirstBlogPage() {
+  return (
+    <div>
+        <h1>First Blog Page</h1>
+    </div>
+  )
+}
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SecondBlogPage() {
+  return (
+    <div>
+        <h1>Second Blog Page</h1>
+    </div>
+  )
+}
